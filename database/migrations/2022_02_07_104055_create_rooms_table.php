6ebbd35240d7f408812d75aa22894ca9a9e7a954
Oskar <oskarzborowski@gmail.com>
2022-02-12 18:53:46 +0100

    Pierwsze propozycje migracji

--- database/migrations/2022_02_07_104055_create_rooms_table.php
+++ database/migrations/2022_02_07_104055_create_rooms_table.php
@@ -17,11 +17,13 @@ class CreateRoomsTable extends Migration
             $table->foreignId('host_id')->references('id')->on('devices')->nullable()->nullOnDelete();
             $table->char('code', 8); // Kodowane automatycznie
             $table->unsignedTinyInteger('game_counter');
-            $table->unsignedTinyInteger('policemen_number')->default(5);
-            $table->unsignedTinyInteger('thieves_number')->default(1);
             $table->enum('game_mode', ['Scotland Yard', 'Mission: Impossible'])->default('Scotland Yard');
             $table->unsignedMediumInteger('game_duration')->default(1800); // czas w sekundach
+            $table->dateTime('end_game')->nullable();
+            $table->unsignedSmallInteger('escape_time')->default(300); // czas w sekundach
             $table->unsignedSmallInteger('disclosures_interval')->default(180); // czas w sekundach
+            $table->unsignedTinyInteger('policemen_number')->default(5);
+            $table->unsignedTinyInteger('thieves_number')->default(1);
             $table->unsignedSmallInteger('catchers_number')->default(2);
             $table->unsignedSmallInteger('catch_radius')->default(50); // dystans w metrach
             $table->unsignedSmallInteger('catch_time')->default(3); // czas w sekundach
@@ -33,12 +35,12 @@ class CreateRoomsTable extends Migration
             $table->polygon('boundary')->nullable();
             $table->multiPoint('mission_centers')->nullable();
             $table->multiPoint('monitoring_centers')->nullable();
-            $table->unsignedSmallInteger('escape_time')->default(300); // czas w sekundach
             $table->boolean('is_direction_visible')->default(true);
+            $table->boolean('is_thief_disclosure_visible')->default(true);
+            $table->boolean('is_catching_visible')->default(true);
             $table->unsignedTinyInteger('black_tickets_number')->default(0);
             $table->unsignedTinyInteger('agents_number')->default(0);
-            $table->dateTime('end_game')->nullable();
-            $table->enum('status', ['WAITING IN ROOM', 'GAME IN PROGRESS', 'GAME OVER'])->default('WAITING IN ROOM');
+            $table->enum('status', ['WAITING IN ROOM', 'GAME IN PROGRESS', 'GAME PAUSED', 'GAME OVER'])->default('WAITING IN ROOM');
             $table->timestamps();
         });
     }
