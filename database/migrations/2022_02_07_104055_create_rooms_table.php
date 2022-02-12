637504bfefc2b1a84de0bdfba068804966f9462c
Oskar <oskarzborowski@gmail.com>
2022-02-12 18:20:20 +0100

    Pierwsze propozycje migracji

--- database/migrations/2022_02_07_104055_create_rooms_table.php
+++ database/migrations/2022_02_07_104055_create_rooms_table.php
@@ -13,28 +13,32 @@ class CreateRoomsTable extends Migration
      */
     public function up() {
         Schema::create('rooms', function (Blueprint $table) {
-            $table->integerIncrements('id');
-            $table->char('code', 8)->unique(); // Kodowane natywnie
-            $table->enum('game_mode', ['STANDARD', 'CHECKPOINT'])->default('STANDARD');
-            $table->enum('disclosure_mode', ['TIME_PERIOD', 'MONITORING'])->default('TIME_PERIOD');
-            $table->boolean('killing_mode')->default(false);
-            $table->unsignedMediumInteger('game_duration')->default(1800); // czas w sekundach
-            $table->unsignedMediumInteger('disclosure_period')->default(300); // czas w sekundach
-            $table->unsignedMediumInteger('killing_time')->default(90); // czas w sekundach
-            $table->unsignedTinyInteger('checkpoints_number')->default(5);
-            $table->unsignedTinyInteger('monitorings_number')->default(5);
-            $table->unsignedTinyInteger('checkpoint_radius')->default(25);
-            $table->unsignedTinyInteger('monitoring_radius')->default(25);
-            $table->unsignedTinyInteger('killing_radius')->default(25);
-            $table->unsignedTinyInteger('catch_radius')->default(25);
+            $table->id();
+            $table->foreignId('host_id')->references('id')->on('devices')->nullable()->nullOnDelete();
+            $table->char('code', 8); // Kodowane automatycznie
+            $table->unsignedTinyInteger('game_counter');
             $table->unsignedTinyInteger('policemen_number')->default(5);
             $table->unsignedTinyInteger('thieves_number')->default(1);
-            $table->multiPoint('checkpoints')->nullable();
-            $table->multiPoint('monitoring')->nullable();
+            $table->enum('game_mode', ['Scotland Yard', 'Mission: Impossible'])->default('Scotland Yard');
+            $table->unsignedMediumInteger('game_duration')->default(1800); // czas w sekundach
+            $table->unsignedSmallInteger('disclosures_interval')->default(180); // czas w sekundach
+            $table->unsignedSmallInteger('catchers_number')->default(2);
+            $table->unsignedSmallInteger('catch_radius')->default(50); // dystans w metrach
+            $table->unsignedSmallInteger('catch_time')->default(3); // czas w sekundach
+            $table->unsignedTinyInteger('missions_number')->default(5);
+            $table->unsignedSmallInteger('mission_radius')->default(50); // dystans w metrach
+            $table->unsignedSmallInteger('mission_time')->default(10); // czas w sekundach
+            $table->unsignedTinyInteger('monitorings_number')->default(0);
+            $table->unsignedSmallInteger('monitoring_radius')->default(50); // dystans w metrach
             $table->polygon('boundary')->nullable();
+            $table->multiPoint('mission_centers')->nullable();
+            $table->multiPoint('monitoring_centers')->nullable();
+            $table->unsignedSmallInteger('escape_time')->default(300); // czas w sekundach
+            $table->boolean('is_direction_visible')->default(true);
+            $table->unsignedTinyInteger('black_tickets_number')->default(0);
+            $table->unsignedTinyInteger('agents_number')->default(0);
             $table->dateTime('end_game')->nullable();
-            $table->enum('status', ['WAITING_IN_ROOM', 'GAME_IN_PROGRESS'])->default('WAITING_IN_ROOM');
-            $table->unsignedSmallInteger('game_counter')->default(0);
+            $table->enum('status', ['WAITING IN ROOM', 'GAME IN PROGRESS', 'GAME OVER'])->default('WAITING IN ROOM');
             $table->timestamps();
         });
     }
