09cc7fb44ccf461904efc34b1fc6715730a90dd1
Oskar <oskarzborowski@gmail.com>
2022-02-16 12:38:13 +0100

    Pierwsze propozycje migracji

--- database/migrations/2022_02_07_104055_create_rooms_table.php
+++ database/migrations/2022_02_07_104055_create_rooms_table.php
@@ -16,51 +16,18 @@ class CreateRoomsTable extends Migration
             $table->id();
             $table->foreignId('host_id')->references('id')->on('devices')->nullable()->nullOnDelete();
             $table->char('code', 8); // Kodowane automatycznie
-            $table->unsignedTinyInteger('game_counter');
+            $table->unsignedSmallInteger('game_counter');
             $table->enum('game_mode', ['Scotland Yard', 'Mission: Impossible'])->default('Scotland Yard');
             $table->json('game_config');
+            $table->polygon('boundary')->nullable();
+            $table->multiPoint('mission_centers')->nullable();
+            $table->multiPoint('missions_completed')->nullable();
+            $table->multiPoint('monitoring_centers')->nullable();
+            $table->multiPoint('monitoring_centrals')->nullable();
             $table->timestamp('game_started_at')->nullable();
             $table->timestamp('game_paused_at')->nullable();
             $table->timestamp('game_ended_at')->nullable();
             $table->timestamp('disclosure_at')->nullable();
-            $table->unsignedSmallInteger('remaining_disclosure_impulses_number')->default(10);
-            $table->unsignedTinyInteger('policemen_number')->default(5);
-            $table->unsignedTinyInteger('agents_number')->default(0);
-            $table->unsignedTinyInteger('saboteurs_number')->default(0);
-            $table->float('saboteur_probability')->default(0);
-            $table->unsignedTinyInteger('thieves_number')->default(1);
-            $table->unsignedTinyInteger('catchers_number')->default(2);
-            $table->unsignedSmallInteger('catch_radius')->default(50); // dystans w metrach
-            $table->unsignedSmallInteger('catch_time')->default(5); // czas w sekundach
-            $table->unsignedTinyInteger('missions_number')->default(5);
-            $table->multiPoint('mission_centers')->nullable();
-            $table->multiPoint('missions_completed')->nullable();
-            $table->unsignedSmallInteger('mission_radius')->default(50); // dystans w metrach
-            $table->unsignedSmallInteger('mission_time')->default(10); // czas w sekundach
-            $table->unsignedTinyInteger('monitorings_number')->default(0);
-            $table->multiPoint('monitoring_centers')->nullable();
-            $table->unsignedSmallInteger('monitoring_radius')->default(50); // dystans w metrach
-            $table->unsignedTinyInteger('monitoring_centrals_number')->default(0);
-            $table->multiPoint('monitoring_centrals')->nullable();
-            $table->unsignedSmallInteger('monitoring_central_radius')->default(50); // dystans w metrach
-            $table->polygon('boundary')->nullable();
-            $table->boolean('is_game_paused_when_disconnected')->default(true);
-            $table->boolean('is_game_paused_after_crossing_border')->default(true);
-            $table->boolean('is_position_shown_after_crossing_border')->default(false);
-            $table->boolean('with_monitoring_central')->default(false);
-            $table->boolean('is_role_random')->default(true);
-            $table->boolean('is_disclosure_interval')->default(true);
-            $table->boolean('is_thief_direction_visible')->default(true);
-            $table->boolean('is_thief_disclosure_visible')->default(true);
-            $table->boolean('is_catching_visible')->default(true);
-            $table->boolean('is_agent_visible')->default(true);
-            $table->unsignedTinyInteger('black_tickets_number')->default(0);
-            $table->unsignedTinyInteger('black_tickets_used_number')->default(0); // przenieść do tabeli players
-            $table->unsignedTinyInteger('white_tickets_number')->default(0);
-            $table->unsignedTinyInteger('white_tickets_used_number')->default(0);
-            $table->unsignedTinyInteger('fake_positions_number')->default(0);
-            $table->unsignedTinyInteger('fake_positions_used_number')->default(0); // przenieść do tabeli players
-            $table->unsignedSmallInteger('fake_position_radius')->default(250); // dystans w metrach
             $table->enum('status', ['WAITING IN ROOM', 'GAME IN PROGRESS', 'GAME PAUSED', 'GAME OVER'])->default('WAITING IN ROOM');
             $table->enum('game_result', ['THIEVES WON ON TIME', 'POLICEMEN WON ON TIME', 'POLICEMEN WON BY CATCHING', 'THIEVES WON BY COMPLETING MISSIONS'])->nullable();
             $table->timestamps();
@@ -77,14 +44,74 @@ class CreateRoomsTable extends Migration
     }
 }
 
+// Struktura JSONa pola "game_config"
 // {
 //     "game_duration": {
 //         "scheduled": 1800,
-//         "real": null
+//         "real": 0
 //     },
 //     "escape_time": 300,
+//     "actor": {
+//         "policeman" : {
+//             "number": 5
+//         },
+//         "thief": {
+//             "number": 1
+//         },
+//         "agent": {
+//             "number": 0
+//         },
+//         "saboteur": {
+//             "number": 0,
+//             "probability": 0
+//         }
+//     },
 //     "disclosure": {
 //         "interval": 180,
-//         "impulses_number": 10
+//         "impulses_number": 10,
+//         "remaining_impulses_number": 10
+//     },
+//     "catching": {
+//         "catchers_number": 2,
+//         "radius": 50,
+//         "time": 5
+//     },
+//     "mission": {
+//         "number": 5,
+//         "radius": 50,
+//         "time": 10
+//     },
+//     "monitoring": {
+//         "number": 0,
+//         "radius": 50,
+//         "central": {
+//             "number": 0,
+//             "radius": 50
+//         }
+//     },
+//     "ticket": {
+//         "black": {
+//             "number": 0,
+//             "used_number": 0
+//         },
+//         "white": {
+//             "number": 0,
+//             "used_number": 0
+//         }
+//     },
+//     "fake_position": {
+//         "number": 0,
+//         "used_number": 0,
+//         "radius": 250
+//     },
+//     "other": {
+//         "is_role_random": true,
+//         "is_thief_direction_visible": true,
+//         "is_thief_disclosure_visible": true,
+//         "is_catching_visible": true,
+//         "is_agent_visible": true,
+//         "is_game_paused_when_disconnected": true,
+//         "is_game_paused_after_crossing_border": true,
+//         "is_position_shown_after_crossing_border": false
 //     }
 // }
