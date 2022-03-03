51f35c8729935af84579ff62c900aa93c6caef7a
Oskar <oskarzborowski@gmail.com>
2022-03-03 09:56:20 +0100

    Stworzenie modeli

--- app/Models/BaseModel.php
+++ app/Models/BaseModel.php
@@ -2,10 +2,11 @@
 
 namespace App\Models;
 
+use App\Http\Traits\Encryptable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class BaseModel extends Model
 {
-    use HasFactory;
+    use Encryptable, HasFactory;
 }
--- app/Models/Room.php
+++ app/Models/Room.php
@@ -4,33 +4,39 @@ namespace App\Models;
 
 class Room extends BaseModel
 {
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
     protected $fillable = [
-        'name',
-        'email',
-        'password',
+        'game_mode',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
     protected $hidden = [
-        'password',
-        'remember_token',
+        'host_id',
+        'street',
+        'city',
+        'voivodeship',
+        'country',
+        'game_counter',
+        'boundary',
+        'mission_centers',
+        'monitoring_centers',
+        'monitoring_centrals',
+        'game_started_at',
+        'game_paused_at',
+        'game_ended_at',
+        'created_at',
+        'updated_at',
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
     protected $casts = [
-        'email_verified_at' => 'datetime',
+        'id' => 'integer',
+        'game_counter' => 'integer',
+        'game_config' => 'array',
+        'game_started_at' => 'string',
+        'game_paused_at' => 'string',
+        'game_ended_at' => 'string',
+        'next_disclosure_at' => 'string',
+    ];
+
+    protected $encryptable = [
+        'code' => 6,
     ];
 }
