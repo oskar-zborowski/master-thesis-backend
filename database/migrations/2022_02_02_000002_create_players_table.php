46fa9844501364d7cf46adb38e4642c62ce31b77
Oskar <oskarzborowski@gmail.com>
2022-03-28 11:48:53 +0200

    Drobne poprawki

--- database/migrations/2022_02_02_000002_create_players_table.php
+++ database/migrations/2022_02_02_000002_create_players_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Libraries\Validation;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -14,23 +15,25 @@ return new class extends Migration
             $table->id();
             $table->foreignId('room_id')->references('id')->on('rooms')->cascadeOnDelete();
             $table->foreignId('user_id')->nullable()->references('id')->on('users')->nullOnDelete();
-            $table->enum('avatar', ['AVATAR_1', 'AVATAR_2', 'AVATAR_3', 'AVATAR_4', 'AVATAR_5']);
-            $table->enum('role', ['POLICEMAN', 'THIEF', 'AGENT', 'SABOTEUR'])->nullable();
+            $table->enum('avatar', Validation::getAvatars());
+            $table->enum('role', Validation::getPlayerRoles())->nullable();
             $table->json('player_config');
-            $table->multiLineString('track')->nullable();
-            $table->multiLineString('disclosure_track')->nullable();
+            $table->lineString('track')->nullable();
+            $table->lineString('disclosure_track')->nullable();
+            $table->point('mission_performed')->nullable();
             $table->multiPoint('missions_completed')->nullable();
             $table->float('direction')->default(0);
-            $table->unsignedTinyInteger('hide_stock')->default(0);
+            $table->unsignedTinyInteger('hide_stock')->default(0); // określa ile odkryć w przód złodziej ma ochronę przed ujawnieniem pozycji
+            $table->boolean('is_fake_position_active')->default(false); // określa czy najbliższa ujawniona pozycja złodzieja ma być fake'owa (już zaktualizowana w disclosure_track)
             $table->boolean('is_bot')->default(false);
-            $table->enum('status', ['DISCONNECTED', 'BORDER_CROSSED', 'BLOCKED'])->nullable();
-            $table->unsignedSmallInteger('average_ping')->default(0); // wyrażony w [ms]
-            $table->unsignedSmallInteger('standard_deviation')->default(0); // wyrażony w [ms]
-            $table->unsignedMediumInteger('samples_number')->default(0);
-            $table->unsignedSmallInteger('expected_time')->default(0); // wyrażony w [ms]
+            $table->enum('status', Validation::getPlayerStatuses())->nullable();
+            $table->unsignedSmallInteger('average_ping')->default(0); // wyrażone w [ms]
+            $table->unsignedSmallInteger('standard_deviation')->default(0); // wyrażone w [ms]
+            $table->unsignedSmallInteger('samples_number')->default(0);
+            $table->timestamp('expected_time_at')->nullable();
+            $table->timestamp('mission_finished_at')->nullable();
             $table->timestamp('catching_finished_at')->nullable();
             $table->timestamp('caught_at')->nullable();
-            $table->timestamp('mission_finished_at')->nullable();
             $table->timestamps();
         });
     }
