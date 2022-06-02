f3363d00ad40efb0c817a2dba7a9e730ad507df8
Oskar <oskarzborowski@gmail.com>
2022-06-02 18:11:17 +0200

    Drobne poprawki

--- app/Http/Controllers/Api/GitHubController.php
+++ app/Http/Controllers/Api/GitHubController.php
@@ -17,8 +17,9 @@ class GitHubController extends Controller
         $githubRepository = env('GITHUB_REPOSITORY');
         $githubBranch = env('GITHUB_BRANCH');
 
-        echo shell_exec('sudo chown -R www-data /var/www/html/master-thesis-beckend');
-        echo shell_exec('sudo chmod 777 -R /var/www/html/master-thesis-beckend');
+        // echo shell_exec('sudo chown -R www-data /var/www/html/master-thesis-beckend');
+        // echo shell_exec('sudo chmod 777 -R /var/www/html/master-thesis-beckend');
+        echo shell_exec('git config --global --add safe.directory /var/www/html/master-thesis-beckend');
         echo shell_exec("git pull https://$githubAccount:$githubToken@github.com/$githubRepository $githubBranch 2>&1");
     }
 }
