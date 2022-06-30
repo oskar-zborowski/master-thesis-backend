63633274e35411a4991c490a175f7c5369073475
Oskar <oskarzborowski@gmail.com>
2022-06-30 13:27:58 +0200

    Drobne poprawki

--- app/Http/Controllers/Api/PlayerController.php
+++ app/Http/Controllers/Api/PlayerController.php
@@ -213,6 +213,9 @@ class PlayerController extends Controller
         $players = $room->players()->where('role', $request->role)->whereIn('status', ['CONNECTED', 'DISCONNECTED'])->get();
         $playersNumber = count($players);
 
+        echo json_encode(strtolower($request->role));
+        die;
+
         if ($playersNumber >= $room->config['actor'][strtolower($request->role)]['number']) {
             throw new ApiException(
                 DefaultErrorCode::FAILED_VALIDATION(),
