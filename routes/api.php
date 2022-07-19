1396fbc8d5ffa4291e47e26e5141ee5204e56f26
Oskar <oskarzborowski@gmail.com>
2022-07-19 22:10:18 +0200

    Drobne poprawki

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/v1/ip-address', function (Request $request) {
-    JsonResponse::sendSuccess($request, ['ip_address' => gethostbyname(gethostname())]);
+    JsonResponse::sendSuccess($request, ['ip_address' => exec("ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'")]);
 })->name('ipAddress-get');
 
 Route::post('/v1/users', [UserController::class, 'createUser'])->name('user-createUser');
