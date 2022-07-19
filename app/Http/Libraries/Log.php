c051d2ecfb514cd7eac975da50aad95f8790ff2b
Oskar <oskarzborowski@gmail.com>
2022-07-19 10:59:30 +0200

    Drobne poprawki

--- app/Http/Libraries/Log.php
+++ app/Http/Libraries/Log.php
@@ -929,8 +929,6 @@ class Log
 
                 sleep(env('NOMINATIM_CONST_PAUSE'));
 
-                FacadesLog::alert('Jestem w nominatim');
-
                 /** @var Config $config */
                 $config = Config::where('id', 1)->first();
                 $isNominatimBusy = $config->is_nominatim_busy || $config->nominatim_last_used_at && Validation::timeComparison($config->nominatim_last_used_at, env('NOMINATIM_CONST_PAUSE'), '<', 'seconds');
